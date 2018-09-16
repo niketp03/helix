f0711479302f15e33826fb3a9aeab7a6245c7abd
Niket Patel <niket_p@outlook.com>
2018-09-15 20:54:35 -0700

    Added pip support
    added support for pip install .

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='helix',
+	version='0.1',
+	description='Genetic Algorithm Framework',
+	author='Niket Patel',
+	author_email='x@example.com',
+	url='https://github.com/MoreHotSauce/helix',
+	packages=['helix'],
+	install_requires=['numpy>=1.14.5',],
+)
